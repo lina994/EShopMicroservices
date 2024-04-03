1cc2aec162e2e9de04694806cd70e3b17ecbba07
Alina Gafarov <alina.g@compie.co.il>
2024-04-03 09:51:42 +0300

    feat: Seeding Initial Data with Marten on PostgresDB for Catalog.API microservice

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -9,5 +9,6 @@ global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Exceptions;
 global using BuildingBlocks.Exceptions.Handler;
 
+global using Catalog.API.Data;
 global using Catalog.API.Exceptions;
 global using Catalog.API.Models;
