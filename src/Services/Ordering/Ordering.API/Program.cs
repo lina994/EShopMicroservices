655b39525642ccb7cbabc30e7931df509aaa33de
Alina Gafarov <alina.g@compie.co.il>
2024-04-06 15:37:37 +0300

    feat: Auto migration and seed initial data

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,8 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,5 +12,12 @@ builder.Services
     .AddApiServices();
 
 var app = builder.Build();
+
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 
 app.Run();
