55ed4903d8211409c6d48e8e94c58d198e9deacf
Alina Gafarov <alina.g@compie.co.il>
2024-04-06 14:38:42 +0300

    feat: Infrastructure configuration
    - Add db context
    - Add entities configuration
    - Add ConnectionStrings
    - Update DependencyInjection
    - Add migration

---
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+global using Ordering.Domain.Enums;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+
+global using Ordering.Infrastructure.Data;
