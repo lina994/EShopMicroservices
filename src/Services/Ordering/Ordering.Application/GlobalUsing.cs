4c947ba4fac2617b21f1b759d7d87526e608ca90
Alina Gafarov <alina.g@compie.co.il>
2024-04-07 13:06:11 +0300

    feat: Add queries - create OrderExtensions - support pagination

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -6,6 +6,7 @@ global using Microsoft.Extensions.Logging;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Pagination;
 
 global using Ordering.Domain.Enums;
 global using Ordering.Domain.Events;
@@ -16,4 +17,4 @@ global using Ordering.Domain.ValueObjects;
 global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
 global using Ordering.Application.Exceptions;
-//global using Ordering.Application.Extensions;
+global using Ordering.Application.Extensions;
