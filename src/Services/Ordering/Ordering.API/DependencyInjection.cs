5cb7397512d8104025321484307966e92874fa07
Alina Gafarov <alina.g@compie.co.il>
2024-04-07 14:11:30 +0300

    feat: Add Carter

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -5,11 +5,13 @@
         public static IServiceCollection AddApiServices
             (this IServiceCollection services)
         {
+            services.AddCarter();
             return services;
         }
-
+        
         public static WebApplication UseApiServices(this WebApplication app)
         {
+            app.MapCarter();
             return app;
         }
     }
