1962658c07c31d6512691a0282e0b6fb1a643e6f
Alina Gafarov <alina.g@compie.co.il>
2024-04-07 12:29:05 +0300

    feat: Create events handlers

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,14 +1,14 @@
 ﻿global using FluentValidation;
-//global using MediatR;
+global using MediatR;
 global using Microsoft.EntityFrameworkCore;
-//global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Logging;
 
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Exceptions;
 
 global using Ordering.Domain.Enums;
-//global using Ordering.Domain.Events;
+global using Ordering.Domain.Events;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
 
