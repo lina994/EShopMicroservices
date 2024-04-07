cc04d7f4fc332de17fbff0251081af15fe740bb2
Alina Gafarov <alina.g@compie.co.il>
2024-04-07 11:34:02 +0300

    feat: Develop IApplicationDbContext for injection EFCore DB Context object in Ordering

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderHandler
+    : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    {
+        public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
